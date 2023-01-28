3cc87be10bc1a2db22ce349cb8bb736d0e575dfc
Oksana Petryshyn <opetryshyn2@gmail.com>
2023-01-28 15:23:46 +0200

    робота на уроці

--- conparison-operators.js
+++ conparison-operators.js
@@ -1,10 +1,12 @@
 // Оператор порівняння
 
-console.log();
+// console.log(5 >= 10);
 
 // Приведення (переобразування) типів до число
 
-console.log();
+// console.log(true > false);
+
+// console.log(Number(15));
 
 
 // Оператори рівності (== і ===) і не рівності (!= і !==)
@@ -13,9 +15,29 @@ console.log();
 let a = 5;
 let b = '5';
 
-console.log();
+// console.log(a != b);
+// console.log(a !== b);
 
+// ...............
+//Оператори порівняння і приведення типов
+// // Яким буде результат цих виразів?
 
+// console.log(5 > 4);
+// console.log(10 >= "7");
+// console.log("2" > "12");
+// console.log("2" < "12");
+// console.log("4" == 4);
+// console.log("6" === 6);
+// console.log("false" === false);
+// console.log(1 == true);
+// console.log(1 === true);
+// console.log("0" == false);
+// console.log("0" === false);
+// console.log("Леопард" < "лев");
+// console.log(undefined == null);
+// console.log(undefined === null);
+
+// console.log(10 === 10);
 
 
 
--- numbers.js
+++ numbers.js
@@ -1,20 +1,23 @@
 // Парс числа з Number.parseInt() 
 // і Number.parseFloat()
 
-const elementWidth = '50px';
-// elementWidth = ;
+let elementWidth = '50px';
+// elementWidth = Number.parseInt(elementWidth);
 // console.log('elementWidth:', elementWidth);
 // ............
 
 let elementHeight = '200.74px';
-// elementHeight = ;
+elementHeight = Number.parseFloat(elementHeight);
 // console.log('elementHeight:', elementHeight);
 
 // ...........
 // Метод число .toFixed(digits)
 // Матрьошки на прикладі console.log(Number(число, toFixed(digits)));
 //
-const salary = 1300.16472;
+let salary = 1300.16472;
+salary = Number(salary.toFixed(3));
+
+// console.log(salary);
 
 // ...........
 
@@ -24,15 +27,18 @@ const salary = 1300.16472;
 let quantity = '30';
 let value = 'Цю строку неможливо перевести в число';
 
+// console.log(Number(value));
+
 // .............
 // Обєкт Math
 // - Піднесення до степені
 // - Exponent operator
 
 const base = 2;
-const power = 5;
+const power = 4;
 
-console.log();
+// const result = Math.pow(base, power);
+// console.log(base ** power);
 
 
 // ..............
@@ -40,6 +46,17 @@ console.log();
 // Генеруємо псевдовипадкове число
 // - Math.random()
 // - Math.round()
+
+// const max = 10;
+// const min = 5;
+
+// const result = Math.round((Math.random() * (max - min) + min));
+
+// console.log(result);
+// console.log(Math.random() * (max - min) + min);
+
+// Number.random() * (max - min) + min;
+
 
 // ..............
 
@@ -56,3 +73,7 @@ console.log();
 // console.log(color);
 
 // document.body.style.backgroundColor = color;
+
+
+
+
